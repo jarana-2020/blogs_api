5142f127ea5467e3e7f6ffb255822f4d4f7306d5
Julio <julioaranabr@yahoo.com.br>
2022-02-04 19:59:20 -0300

    create getemail in user service

--- Services/User.js
+++ Services/User.js
@@ -1,13 +1,24 @@
 const { User } = require('../models');
 
+const getEmail = async (email) => {
+  const user = await User.findOne({
+    where: { email },
+  });
+  return user;
+};
+
 const createUser = async (userObj) => {
   const { displayName, email, password, image } = userObj;
+  const userEmail = await getEmail(userObj.email);
+  if (userEmail) {
+    return { code: 409, 
+      message: { message: 'User already registered' } };
+  }
   const user = await User.create({
     displayName,
     email,
     password,
-    image,
-  });
+    image });
   return user;
 };
 
