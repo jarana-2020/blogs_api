b6ffeeaca9a8ecf1df37ccfc90ecf54baa7681aa
Julio <julioaranabr@yahoo.com.br>
2022-02-04 16:34:13 -0300

    create model user

---
+++ models/User.js
@@ -0,0 +1,10 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    displayName: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING },
+    image: { type: DataTypes.STRING },
+  });
+  return User;
+};
