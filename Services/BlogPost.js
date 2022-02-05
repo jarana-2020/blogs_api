93b1db908d33b2f6565672ba9489f6d81f234de0
Julio <julioaranabr@yahoo.com.br>
2022-02-05 18:16:44 -0300

    blog post model and service

---
+++ Services/BlogPost.js
@@ -0,0 +1,49 @@
+const { BlogPost, PostsCategorie, Categorie } = require('../models');
+
+const categorieExists = (array) => {
+   const result = array.reduce(async (ac, cur) => {
+     const categorie = await Categorie.findByPk(cur);
+     if (!categorie) {
+      return { code: 400, 
+        message: { message: '"categoryIds" not found' } };
+     }
+     return ac;
+   }, false);
+   return result;
+};
+
+const insertPost = async (title, content, userId) => {
+  const post = await BlogPost.create({
+    title,
+    content,
+    userId,
+  });
+  return post;
+};
+
+const insertPostCategorie = (arrayCategories, postId) => {
+  Promise.all(arrayCategories.map(async (id) => {
+    await PostsCategorie.create({
+          postId,
+          categoryId: id,
+        });
+  }));
+};
+
+const createPost = async (objPost, userId) => {
+  const { title, content, categoryIds } = objPost;
+  const verifyCategory = await categorieExists(categoryIds);
+  if (verifyCategory.message) return verifyCategory;
+  const post = await insertPost(title, content, userId);
+  insertPostCategorie(categoryIds, post.id);
+  return {
+    id: post.id,
+    userId,
+    title,
+    content,
+  };
+};
+
+module.exports = {
+  createPost,
+};
--- models/BlogPost.js
+++ models/BlogPost.js
@@ -4,10 +4,11 @@ module.exports = (sequelize, DataTypes) => {
     title: { type: DataTypes.STRING },
     content: { type: DataTypes.STRING },
     userId: { type: DataTypes.INTEGER, foreignKey: true },
-    createdAt: { type: DataTypes.DATE },
-    updatedAt: { type: DataTypes.DATE },
+    published: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    updated: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
   },
   {
+    timestamps: false,
     tableName: 'BlogPosts',
   });
   BlogPost.associate = (models) => {
