3b171e11f81bc23bb39ae2f58c4d1f80e14dfd08
Julio <julioaranabr@yahoo.com.br>
2022-02-05 18:17:25 -0300

    blog post controller

---
+++ Controllers/BlogPost.js
@@ -0,0 +1,18 @@
+const ServiceBlogPost = require('../Services/BlogPost');
+
+const createPost = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const post = await ServiceBlogPost.createPost(req.body, id);
+    if (post.message) {
+      return res.status(post.code).json(post.message);
+    }
+    return res.status(201).json(post);
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  createPost,
+};
