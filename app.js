a566586dfbefa6098bb20812f4969531572c4ae0
Julio <julioaranabr@yahoo.com.br>
2022-02-14 22:01:04 -0300

    refatored app.js

--- app.js
+++ app.js
@@ -1,36 +1,18 @@
 const express = require('express');
-const UserController = require('./Controllers/User');
-const LoginController = require('./Controllers/Login');
-const CategorieController = require('./Controllers/Categorie');
-const PostController = require('./Controllers/BlogPost');
+const userRouter = require('./routes/User');
+const categorieRouter = require('./routes/Categorie');
+const loginRouter = require('./routes/Login');
+const postRouter = require('./routes/Post');
 
-const {
-  checkFieldExists,
-  validateFields,
-} = require('./Middlewares/validateUser');
-
-const { checkLogin } = require('./Middlewares/validateLogin');
-const { checkToken } = require('./Middlewares/validateToken');
-const { validateName } = require('./Middlewares/validateCategorie');
-const { checkPost, checkEditPost } = require('./Middlewares/validatePost');
 const errorMidleware = require('./Middlewares/error');
 
 const app = express();
 app.use(express.json());
 
-app.post('/user', checkFieldExists, validateFields, UserController.createUser);
-app.post('/categories', checkToken, validateName, CategorieController.createCategorie);
-app.post('/login', checkLogin, LoginController.executeLogin);
-app.post('/post', checkToken, checkPost, PostController.createPost);
-app.get('/user', checkToken, UserController.getAllUsers);
-app.get('/categories', checkToken, CategorieController.getAllCategories);
-app.get('/post', checkToken, PostController.getAllPosts);
-app.delete('/user/me', checkToken, UserController.deleteUser);
-app.get('/post/search', checkToken, PostController.searchPost);
-app.put('/post/:id', checkToken, checkEditPost, PostController.editPost);
-app.get('/user/:id', checkToken, UserController.userById);
-app.get('/post/:id', checkToken, PostController.getPostById);
-app.delete('/post/:id', checkToken, PostController.deletePost);
+app.use('/user', userRouter);
+app.use('/categories', categorieRouter);
+app.use('/login', loginRouter);
+app.use('/post', postRouter);
 app.use(errorMidleware);
 
 module.exports = app;
