2c50a980091e875c40bd41cc381d3199f149b3ad
Julio <julioaranabr@yahoo.com.br>
2022-03-02 16:22:06 -0300

    refactored config js

--- config/config.js
+++ config/config.js
@@ -1,12 +1,32 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+/* eslint-disable camelcase */
+require('dotenv/config');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      rejectUnauthorized: false,
-    },
+const { HOST, PASSWORD_POSTGRES, DATABASE, DB_USERNAME, DB_PORT, DATABASE_URL } = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
   },
-});
-
-module.exports = sequelize;
+  test: {
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    dialect: 'postgres',
+  },
+  production: {
+    username: DB_USERNAME,
+    password: PASSWORD_POSTGRES,
+    database: DATABASE,
+    host: HOST,
+    port: DB_PORT,
+    use_env_variable: DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
