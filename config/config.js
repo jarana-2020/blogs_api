f1e507e5854ab4b520839906b46aff668fa64b98
Julio <julioaranabr@yahoo.com.br>
2022-03-02 14:05:10 -0300

    refactored config js

--- config/config.js
+++ config/config.js
@@ -2,24 +2,23 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+        ssl: true,
+    },
   },
   test: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
   production: {
-    username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: 'blogs_api',
-    host: process.env.HOSTNAME,
-    dialect: 'mysql',
+    url: process.env.DATABASE_URL,
+        dialect: 'postgres',
+        ssl: true,
+        dialectOptions: {
+            ssl: true,
+        },
   },
 };
